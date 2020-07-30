72f7718ae562a1ff09818bed266a94c65bc4a2b8
Vicario Williams <vawilliams@github.com>
2020-07-30 21:16:22 +0200

    Updates

---
+++ precache-manifest.ba6d78834ff71fda0f2af714079348c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25103da78512ca1b2146d01bdbacff7",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "15c432a7695291d90966",
+    "url": "/Markdown-Previewer/static/css/2.60a4c9f7.chunk.css"
+  },
+  {
+    "revision": "98b67f410954fa629f56",
+    "url": "/Markdown-Previewer/static/css/main.23c4a2ff.chunk.css"
+  },
+  {
+    "revision": "15c432a7695291d90966",
+    "url": "/Markdown-Previewer/static/js/2.e665fc9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.e665fc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b67f410954fa629f56",
+    "url": "/Markdown-Previewer/static/js/main.7c74ab10.chunk.js"
+  },
+  {
+    "revision": "b0a69d01708cd88e3db1",
+    "url": "/Markdown-Previewer/static/js/runtime-main.baa23fdd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Markdown-Previewer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Markdown-Previewer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Markdown-Previewer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Markdown-Previewer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Markdown-Previewer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
